b0d748df799318ed95e8a08c0a0dc5c7a1d53f10
jrswab <jrswab@gmail.com>
2018-12-31 12:20:22 -0500

    added missing wget

--- config/install.sh
+++ config/install.sh
@@ -4,8 +4,8 @@ if [ ! -f ~/.smoke/smoked ]; then
 	cd ~/.smoke
 
 	# download smoked and wallet
-	https://github.com/smokenetwork/smoked/releases/download/v0.0.6/smoked-0.0.6-x86_64-linux.tar.gz
-	https://github.com/smokenetwork/smoked/releases/download/v0.0.6/cli_wallet-0.0.6-x86_64-linux.tar.gz
+	wget https://github.com/smokenetwork/smoked/releases/download/v0.0.6/smoked-0.0.6-x86_64-linux.tar.gz
+	wget https://github.com/smokenetwork/smoked/releases/download/v0.0.6/cli_wallet-0.0.6-x86_64-linux.tar.gz
 	
 	# extract smoked and wallet
 	tar -xzf smoked-0.0.6-x86_64-linux.tar.gz
